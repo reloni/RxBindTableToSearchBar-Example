391f848e0a3e0c589d970647a997179988c89583
Anton Efimenko <a.efimenko@alpinadigital.ru>
2020-06-07 14:04:43 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Anton Efimenko on 07.06.2020.
+//  Copyright © 2020 Anton Efimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
